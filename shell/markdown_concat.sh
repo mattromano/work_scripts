1bf9e5ba442f890e14ff662e0b9f4c4719ba326f
mattromano <mattromano88@gmail.com>
2023-11-03 11:34:23 -0700

    add log parser script

---
+++ shell/markdown_concat.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# Path to the directory containing markdown files
+dir_path="/Users/mattromano/Desktop/repos/ethereum-models/models/doc_descriptions/aave"
+
+# Output file
+output_file="/Users/mattromano/Desktop/repos/ethereum-models/models/doc_descriptions/complete_lending_docs.md"
+
+# Remove the output file if it already exists
+rm -f "$output_file"
+
+# Loop through all markdown files in the specified directory
+for file in "$dir_path"/*.md; do
+    # Check if the file exists and is a regular file
+    if [[ -f "$file" ]]; then
+        # Concatenate the content of the current file to the output file
+        cat "$file" >> "$output_file"
+        # Add a blank line after the content of the current file
+        echo "" >> "$output_file"
+    fi
+done
+
+echo "Markdown files concatenated into $output_file"
